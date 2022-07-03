fea0fbee428647735f797e5410d04eb6508b7949
GytisKlnt <gytis.kalanta@gmail.com>
2022-07-03 15:45:21 +0300

    created Login page with fetch

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../components/Container/Container";
 import Hero from "../components/Hero/Hero";
 import LoginForm from "../components/LoginForm/LoginForm";
+import Notification from "../components/Notification/Notification";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [error, setError] = useState();
+
+  const navigate = useNavigate();
+
+  const loginUser = async (inputs) => {
+    try {
+      const res = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/v1/users/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(inputs),
+        }
+      );
+
+      const data = await res.json();
+
+      if (data.err) {
+        return setError(data.err);
+      }
+
+      localStorage.setItem("token", data.token);
+      console.log(localStorage.getItem("token"));
+      navigate("/home");
+
+      // return setError(data.msg);
+    } catch (error) {
+      return setError(error.msg);
+    }
+  };
+
   return (
     <Container>
       <Hero title="Basketball Agent" subtitle="Find your Team or Teammate" />
-      <LoginForm handleSubmit={(values) => console.log(values)} />
+      <LoginForm handleSubmit={loginUser}>
+        {error && <Notification>{error}</Notification>}
+      </LoginForm>
     </Container>
   );
 };
